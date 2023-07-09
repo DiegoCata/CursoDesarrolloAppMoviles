9ccab7c89726daf37cfbac6dae8df8d9fbbd75bf
Diego Cata <difercata26@gmail.com>
2023-07-08 22:04:00 -0500

    SubidaDelProyecto

---
+++ app/src/main/java/com/diego/cursodesarrolloaplicacionesmoviles/firstApp/FirstAppActivity.kt
@@ -0,0 +1,12 @@
+package com.diego.cursodesarrolloaplicacionesmoviles.firstApp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.diego.cursodesarrolloaplicacionesmoviles.R
+
+class FirstAppActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_first_app)
+    }
+}
