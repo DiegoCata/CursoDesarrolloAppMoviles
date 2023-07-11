12e40d0dd8d004698a50b5578b0da4b8e9bf3066
Diego Cata <difercata26@gmail.com>
2023-07-11 12:45:37 -0500

    Creación Menu

--- app/src/main/java/com/diego/cursodesarrolloaplicacionesmoviles/CarpertaPrincipal/FirstApp/FirstAppActivity.kt
+++ app/src/main/java/com/diego/cursodesarrolloaplicacionesmoviles/CarpertaPrincipal/FirstApp/FirstAppActivity.kt
@@ -1,4 +1,4 @@
-package com.diego.cursodesarrolloaplicacionesmoviles.firstApp
+package com.diego.cursodesarrolloaplicacionesmoviles.CarpertaPrincipal.FirstApp
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
--- app/src/main/java/com/diego/cursodesarrolloaplicacionesmoviles/CarpertaPrincipal/FirstApp/ResultActivity.kt
+++ app/src/main/java/com/diego/cursodesarrolloaplicacionesmoviles/CarpertaPrincipal/FirstApp/ResultActivity.kt
@@ -1,4 +1,4 @@
-package com.diego.cursodesarrolloaplicacionesmoviles.firstApp
+package com.diego.cursodesarrolloaplicacionesmoviles.CarpertaPrincipal.FirstApp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
