e27e5d444ae60d41e825d327c04230dc9a964a3d
Diego Cata <difercata26@gmail.com>
2023-07-11 10:08:54 -0500

    Creación primer Activity

--- app/src/main/java/com/diego/cursodesarrolloaplicacionesmoviles/firstApp/FirstAppActivity.kt
+++ app/src/main/java/com/diego/cursodesarrolloaplicacionesmoviles/firstApp/FirstAppActivity.kt
@@ -1,12 +1,17 @@
 package com.diego.cursodesarrolloaplicacionesmoviles.firstApp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatEditText
 import com.diego.cursodesarrolloaplicacionesmoviles.R
 
 class FirstAppActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first_app)
+        val btnStart = findViewById<AppCompatButton>(R.id.btnStart)
+        val etName = findViewById<AppCompatEditText>(R.id.etName)
     }
 }
