9ed9d01d52ae9476d47034fdd1c73d9a5abb7fc5
Diego Cata <difercata26@gmail.com>
2023-07-11 21:30:29 -0500

    App Calculo IMC

--- app/src/main/java/com/diego/cursodesarrolloaplicacionesmoviles/MenuActivity.kt
+++ app/src/main/java/com/diego/cursodesarrolloaplicacionesmoviles/MenuActivity.kt
@@ -5,17 +5,26 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import com.diego.cursodesarrolloaplicacionesmoviles.CarpertaPrincipal.FirstApp.FirstAppActivity
+import com.diego.cursodesarrolloaplicacionesmoviles.imccalculator.ImcCaculatorActivity
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
         val btnSaludApp = findViewById<Button>(R.id.btnSaludApp)
+        val btnImcApp = findViewById<Button>(R.id.btnIMCApp)
+
         btnSaludApp.setOnClickListener { navigateToSaludApp() }
+        btnImcApp.setOnClickListener { navigateToImcApp() }
     }
 
     private fun navigateToSaludApp(){
         val intent = Intent(this, FirstAppActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun navigateToImcApp() {
+        val intent = Intent(this, ImcCaculatorActivity::class.java)
         startActivity(intent)
     }
 
